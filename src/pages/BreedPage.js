340b92ca6be7a368b3672f8ee039983b2984362f
ory <oooory@gmail.com>
2021-01-04 23:23:24 +0200

    filter-bar is set

--- src/pages/BreedPage.js
+++ src/pages/BreedPage.js
@@ -1,30 +1,42 @@
 import { useEffect, useState } from "react"
 import axios from 'axios';
 import BreedCard from "../BreedCard";
-
+import { Container, Form, Row } from "react-bootstrap";
+import './BreedPage.css';
 
 
 function BreedPage() {
     const [breedsList,setBreedsList]= useState([]);
-    
+    const [filter, setFilter]= useState("");
 
     useEffect(()=>{
         axios.get("https://dog.ceo/api/breeds/list/all")
         .then(response => setBreedsList(Object.keys(response.data.message)));
         // gives a list of strings with 94 dog's-breeds (breedsList)
         },[])
+    // turn all the list of breeds into a list of componnents that gets prop of thier breed name:
+    let breedCard= breedsList.map((breed, index)=> <BreedCard breed={breed} key={index}/>)
     
-    const dogCards= breedsList.map((breed, index)=> <BreedCard breed={breed} key={index}/>)
-    
-    
+    // filtering for user's filter:
+    if (breedsList !== []) {
+        const filteredResult = breedsList.filter(item => item.includes(filter.toLowerCase()));
+        breedCard= filteredResult.map(breed=> <BreedCard breed={breed}/>)
+        }
+ 
         
 
         return(
-            <div className="container">
-                {dogCards}
-            </div>
+            <Container>
+                <div className="form-filter">
+                    <input type="text" placeholder="Filter Dogs..." id="filter" value={filter} 
+                        onChange={(e)=> setFilter(e.target.value)}/>
+                </div>
+                    {breedCard}
+                
+                
+            </Container>
         
     )
     
 }
-export default BreedPage;
+export default BreedPage
